32e2cd0e1afb8744b309d7d8e8f42e8f3c644d4b
TaibkKurbanaliev <65422168+TaibkKurbanaliev@users.noreply.github.com>
2025-01-02 21:06:29 +0400

    Add cmakelist and precompiled header

---
+++ Elysium/Source.cpp
@@ -0,0 +1,6 @@
+#include "src/epch.h"
+
+int main() 
+{
+	return 0;
+}
---
+++ Elysium/src/epch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+#include <memory>
+#include <vector>
+#include <functional>
+#include <string>
+#include <numeric>
+
+
+#ifdef ELYSIUM_PLATFORM_WINDOWS
+	#include <Windows.h>
+#endif
