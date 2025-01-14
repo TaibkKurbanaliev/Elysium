3e19a0b1c44b7111dc8ce442a3ccd432ae64b7a6
TaibkKurbanaliev <65422168+TaibkKurbanaliev@users.noreply.github.com>
2025-01-14 22:20:45 +0400

    Include glad

--- Elysium/src/Window.cpp
+++ Elysium/src/Window.cpp
@@ -1,13 +1,33 @@
 #include "Window.h"
+#include "imgui.h"
+#include ""
 
 namespace Elysium
 {
 	Window::Window(const WindowProps& props)
 	{
+		if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD))
+		{
+			printf("Error: SDL_Init(): %s\n", SDL_GetError());
+		}
+
 		m_Window = SDL_CreateWindow(props.Title.c_str(), 
 									props.Width, 
 									props.Height, 
 									SDL_WINDOW_RESIZABLE);
+		ImGui::CreateContext();
+		ImGuiIO& io = ImGui::GetIO(); (void)io;
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+		// Setup Dear ImGui style
+		ImGui::StyleColorsDark();
+		//ImGui::StyleColorsLight();
+
+		// Setup Platform/Renderer backends
+		ImGui_ImplSDL3_InitForOpenGL(window, gl_context);
+		ImGui_ImplOpenGL3_Init(glsl_version);
+
 	}
 
 	Window::~Window()
--- Elysium/src/Window.h
+++ Elysium/src/Window.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "SDL3/SDL.h"
-#include "imgui.h"
+
 #include "Elysium/Core.h"
 #include "epch.h"
 
